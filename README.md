9d97a824da5eebb086b3d8c3d4c336d18f4625fc
Kevin Gawlik <kevin@gawlik.io>
2020-05-01 17:17:05 +0200

    init: first commit

---
+++ README.md
@@ -0,0 +1,119 @@
+# svelte-class-transition
+
+`svelte-class-transition` is a very simple Svelte 3 component that allows you to do transition using CSS classes.
+
+## Install
+
+Using `yarn`:
+
+```
+yarn add svelte-class-transition
+```
+
+Using `npm`:
+
+```
+npm install svelte-class-transition
+```
+
+## Usage
+
+### Examples
+
+Here's how it should be used:
+
+```html
+<script>
+    import Transition from 'svelte-class-transition';
+</script>
+
+<Transition
+    {toggle}
+    transitions="transition transform"
+    inTransition="ease-out duration-1000"
+    inState="opacity-0 -translate-x-16"
+    onState="opacity-100 translate-x-0"
+    outState="opacity-0 translate-x-16"
+    outTransition="ease-in duration-1000"
+>
+    <!-- All the classes will be applied to the slot's main element, in this case, the div below -->
+    <div>
+        ...
+    </div>
+</Transition>
+```
+
+You can find multiple examples [here](https://svelte-class-transition-examples.now.sh/).
+
+### Props
+
+`svelte-class-transition` has several props for different use cases. All of them are optionnal but without any, the component won't do anything.
+
+#### `toggle`
+
+Type: `boolean | undefined`
+
+Default value: `undefined`
+
+The `toggle` property is where you'll store your toggling variable. If set to `true` the `slot` will be first visible, if set to `false` it will start hidden, you should note that there is **no animation for the first state**. On change of the property, the `slot` will animate according to the other props.
+
+There is a possiblity to not set this property:
+
+- If the component has a parent (or grand*-parent) transition component, it will listen to its changes and react according to them. The parent (or grand*-parent) transition component will wait until all the animations are finished before being hidden.
+- If the component doesn't have a parent (or grand*-parent) transition component, it will show the slot **with an animation once the DOM has fully loaded**.
+
+#### `inState`
+
+Type: `string`
+
+Default value: `''`
+
+The `inState` property is the state from which the `slot` while come from when setting the `toggle` to `true`. If you want your component to go from `opacity-0` to `opacity-100`, you should set `inState` to `opacity-0`.
+
+#### `onState`
+
+Type: `string`
+
+Default value: `''`
+
+The `onState` property is the state from which the `slot` while go to when setting the `toggle` to `true`. If you want your component to go from `opacity-0` to `opacity-100`, you should set `onState` to `opacity-100`.
+
+#### `outState`
+
+Type: `string`
+
+Default value: `inState`
+
+The `outState` property is the state from which the `slot` while go to when setting the `toggle` to `false`. If you want your component to go from `opacity-100` to `opacity-0`, you should set `outState` to `opacity-0`. If you want the same `inState` and `outState`, you can skip this prop.
+
+#### `inTransition`
+
+Type: `string`
+
+Default value: `''`
+
+The `inTransition` property is the transition the `slot` will use after `toggle` has been set to `true`. You'll want to set here the transition, the easing, the duration and eventually the delay. For example it could be: `transition ease-out duration-300`.
+
+#### `outTransition`
+
+Type: `string`
+
+Default value: `inTransition`
+
+The `outTransition` property is the transition the `slot` will use after `toggle` has been set to `false`. You'll want to set here the transition, the easing, the duration and eventually the delay. For example it could be: `transform ease-in duration-200`. If you want the same `inTransition` and `outTransition`, you can skip this prop.
+
+#### `transitions`
+
+Type: `string`
+
+Default value: `''`
+
+The `transitions` property is where you'll regroup your common classes from `inTranstion` and `outTransition`, like for example the transition property. It's totally optionnal and doesn't change anything inside the component, it's here to make your code look cleaner.
+
+#### `offVisible`
+
+Type: `string`
+
+Default value: `true`
+
+The `offVisible` property is where you'll define if the `slot` should be `hidden` when off or not.
---
+++ Transition.svelte
@@ -0,0 +1,145 @@
+<script>
+    import { onMount } from 'svelte';
+
+    export let toggle = undefined;
+    export let transitions = '';
+    export let inTransition = '';
+    export let outTransition = inTransition;
+    export let inState = '';
+    export let onState = '';
+    export let outState = inState;
+    export let offVisible = false;
+
+    let div;
+    let slot;
+    let slotClasses;
+    let parent;
+
+    const STATE = {
+        IDLE: 0,
+        ENTERING: 1,
+        LEAVING: 2,
+    };
+
+    let state = STATE.IDLE;
+
+    onMount(() => {
+        slot = div.nextElementSibling;
+        slotClasses = slot.classList.value;
+
+        if (toggle === undefined) {
+            slot.hidden = true;
+            if (document.readyState === 'complete') {
+                initTogglelessTransition();
+            } else {
+                document.addEventListener('DOMContentLoaded', () => {
+                    initTogglelessTransition();
+                }, { once: true });
+            }
+        } else {
+            initTransition();
+        }
+    });
+
+    const initTogglelessTransition = () => {
+        searchParentTransition();
+        toggle = parent ? parent.toggle : false;
+        initTransition();
+        if (! parent) {
+            setTimeout(() => {
+                toggle = true;
+            }, navigator.userAgent.toLowerCase().includes('firefox') ? 200 : 50);
+        }
+    }
+
+    const searchParentTransition = () => {
+        let element = slot.parentElement;
+        while (
+            parent === undefined
+            && element
+            && document.body !== element
+        ) {
+            if (element.toggle !== undefined) {
+                parent = element;
+                parentObserver();
+            } else {
+                element = element.parentElement;
+            }
+        }
+    }
+
+    const parentObserver = () => {
+        new MutationObserver((mutations) => {
+            for (let mutation of mutations) {
+                toggle = mutation.target.toggle;
+            }
+        }).observe(parent, {
+            attributes: true,
+            attributeFilter: [ 'class' ]
+        });
+    }
+
+    const initTransition = () => {
+        slot.toggle = toggle;
+
+        if (toggle) {
+            slot.classList.value = slotClasses + ' ' + transitions + ' ' + outTransition + ' ' + onState;
+            transitionEndListener();
+        } else {
+            slot.hidden = ! offVisible && ! parent;
+            slot.classList.value = slotClasses + ' ' + inState;
+            setTimeout(() => {
+                slot.classList.value = slot.classList.value + ' ' + transitions + ' ' + inTransition;
+                transitionEndListener();
+            }, 250);
+        }
+    }
+
+    const transitionEndListener = () => {
+        slot.addEventListener('transitionend', (event) => {
+            if (
+                (inTransition === '' || event.target === slot)
+                && ((toggle && state === STATE.ENTERING) || (! toggle && state === STATE.LEAVING))
+            ) {
+                state = STATE.IDLE;
+                if (! toggle) {
+                    slot.hidden = ! offVisible && ! parent;
+                    slot.classList.value = slotClasses + ' ' + transitions + ' ' + inTransition + ' ' + inState;
+                }
+            }
+        });
+    }
+
+    let initialized = false;
+    let firstToggleState = toggle;
+    $: firstToggleState !== toggle && (initialized = true);
+    $: initialized && event(toggle);
+
+    const event = (toggle) => {
+        slot.toggle = toggle;
+        toggle ? enterEvent() : leaveEvent();
+    }
+
+    const enterEvent = () => {
+        if (slot.hidden) {
+            slot.hidden = false;
+            setTimeout(() => {
+                enterEvent();
+            }, 50);
+        } else {
+            state = STATE.ENTERING;
+            slot.classList.value = slotClasses + ' ' + transitions + ' ' + inTransition + ' ' + onState;
+        }
+    };
+
+    const leaveEvent = () => {
+        state = STATE.LEAVING;
+        slot.classList.value = slotClasses + ' ' + transitions + ' ' + outTransition + ' ' + outState;
+    }
+</script>
+
+<div
+    bind:this={div}
+    hidden
+/>
+<slot />
